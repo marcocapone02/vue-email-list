f9e04bded6453e1caf972a7ffdd5a378cfbfe887
Marco Capone <118698117+marcocapone02@users.noreply.github.com>
2023-04-26 18:25:13 +0200

    Api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,22 @@
 const { createApp } = Vue;
 
 createApp({
-  
+  data() {
+    return{
+      title:'Email List'
+      apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail'
+    }
+  },
+
+  methods:{
+    getApi(){
+      axios.get(this.apiUrl)
+      .then( res => {
+        console.log(res);
+      })
+    }
+  },
+  mounted() {
+    this.getApi()
+  },
 }).mount('#app')
